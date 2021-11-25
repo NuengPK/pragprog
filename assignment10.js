17175d710b94f9676cbc753c80a557eb6f86698a
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-25 16:21:46 +0700

    assignment10

---
+++ assignment10.js
@@ -0,0 +1,6 @@
+var enterText1;
+enterText1 = prompt(" คําคม หรือ คําพังเพย อะไร?","กรอกคำคม");
+
+var enterText2;
+enterText2 = prompt("ใครเป็นคนกล่าวไว้ ?","โปรดระบุชื่อ");
+console.log(enterText2,"กล่าวว่า","\"",enterText1,"\"");
