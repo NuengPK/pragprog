1daf9803caaee43db05866fbc149c877beeb7ea1
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-25 18:01:04 +0700

    assignment11

---
+++ assignment11.js
@@ -0,0 +1,16 @@
+var enterText3;
+enterText3 = prompt("Enter a noun?","eater");
+
+var enterText4;
+enterText4 = prompt("Enter a verb?","eater");
+
+var enterText5;
+enterText5 = prompt("Enter an adjective?","eater");
+
+var enterText6;
+enterText6 = prompt("Enter an adverb?","eater");
+
+
+console.log("Enter a noun?",enterText3,"\n","Enter a verb?",enterText4,"\n","Enter an adjective?"
+            ,enterText5,"\n","Enter an adverb?",enterText6,"\n",
+            "Do you ",enterText4," your ",enterText5,enterText3,enterText6,"\?","นี่แหละที่มันฮา!");
