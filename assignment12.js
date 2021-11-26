a4b985bff2f6bcfbdf0f10a43dc69ce128774a68
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-26 13:08:16 +0700

    ลบ newfuntion ออก

--- assignment12.js
+++ assignment12.js
@@ -5,12 +5,10 @@ var enterText9;
 enterText9 =Number(prompt("What is the second number?"));
 console.log("\n","What is the first number",enterText8,"\n","What is the second number",enterText9);
 
-newFunction();
 
-function newFunction() {
-    console.log(enterText8,'+',enterText9,'=',(enterText8 + enterText9));
-    console.log(enterText8,'-',enterText9,'=',(enterText8 - enterText9));
-    console.log(enterText8,'*',enterText9,'=',(enterText8 * enterText9));
-    console.log(enterText8,'/',enterText9,'=',(enterText8 / enterText9));
-}
+    console.log("\n",enterText8,'+',enterText9,'=',(enterText8 + enterText9),"\n"
+    ,enterText8,'-',enterText9,'=',(enterText8 - enterText9),"\n"
+    ,enterText8,'*',enterText9,'=',(enterText8 * enterText9),"\n"
+    ,enterText8,'/',enterText9,'=',(enterText8 / enterText9),"\n");
+
 
