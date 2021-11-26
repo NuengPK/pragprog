30c032251b3622e0ea4ef8540c98aa3821a65b0c
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-26 16:30:23 +0700

    แก้ไข ลบกรอกคำคมออกเนื่องจากพิมผิด assignment12

--- assignment12.js
+++ assignment12.js
@@ -1,8 +1,8 @@
 var enterText8;
-enterText8 = Number(prompt("What is the first number?","กรอกคำคม"));
+enterText8 = Number(prompt("What is the first number?","num"));
 
 var enterText9;
-enterText9 =Number(prompt("What is the second number?"));
+enterText9 =Number(prompt("What is the second number?","num"));
 console.log("\n","What is the first number",enterText8,"\n","What is the second number",enterText9);
 
 
