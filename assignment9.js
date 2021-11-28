c387fe05296ce555d151c02dae8b3092a5a1b4c2
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-28 20:31:03 +0700

    edit

--- assignment9.js
+++ assignment9.js
@@ -1,2 +1,4 @@
-var numOfCharacters = enterText.length;
-console.log(enterText," has ",numOfCharacters," characters.");
+var enterText0;
+enterText0 = prompt("What is the input string?","string");
+var numOfCharacters = enterText0.length;
+console.log(enterText0," has ",numOfCharacters," characters.");
