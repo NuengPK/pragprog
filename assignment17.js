90fe8b7bb4c8e21f8d763b8f113a31bdd8793236
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-28 21:04:15 +0700

    assignment17

---
+++ assignment17.js
@@ -0,0 +1,17 @@
+var painT;
+painT = 350
+
+var f1;
+f1 = Number(prompt());
+
+var f2;
+f2 = Number(prompt());
+
+var f3;
+f3 = (f1 * f2) ;
+
+var f4;
+f4 = Math.ceil(f3 / painT) ;
+
+console.log ("You will need to purchase ",f4
+," gallons of paint to cover ",f3," square feet.");
