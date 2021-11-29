8bb072a8d1ae27dd54147db416db2455ebef2c45
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-29 17:33:46 +0700

    assignment20

---
+++ assignment20.js
@@ -0,0 +1,14 @@
+var PP;
+PP = prompt("Enter the principal:");
+var RT;
+RT = prompt("Enter the rate of interest:");
+var NY;
+NY = prompt("Enter the number of years:");
+var invesTment;
+
+invesTment = ((PP * ( RT* NY )) / 100) + 1500;
+
+console.log("Enter the principal: ",PP,"\n","."
+,",Enter the rate of interest: ",NY,"\n","."
+,"Enter the number of years: ",RT,"\n","."
+,"After ",NY," years at ",RT,"%, the investment will be worth ",Math.ceil(invesTment),".");
