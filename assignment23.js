8bab0b58923acb980910ee675f2f4caa08e14c1e
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-12-02 17:08:51 +0700

    assignment23

---
+++ assignment23.js
@@ -0,0 +1,3 @@
+let age = Number(prompt("อายุ"));
+let yourAge = (age >= 18) ? "คุณอายุมากพอที่จะขับรถอย่างถูกกฎหมาย" : "คุณอายุไม่ถึงเกณฑ์ที่จะขับรถอย่างถูกกฎหมาย";
+console.log("What is your age? "+age+"\n",yourAge);
