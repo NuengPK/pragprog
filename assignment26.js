88488d3440b07427200a2882d95a61ef7550ba48
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-12-09 13:02:01 +0700

    edit if(cm)

--- assignment26.js
+++ assignment26.js
@@ -1,9 +1,14 @@
 let cm = Number(prompt("ส่วนสูง"));
-let kg = Number(prompt("น้ำหนัก"));
 let bmi;
-bmi = (kg / (cm*cm))*10000
-let okBmi = (bmi<= 18.6) ? "you are too skinny You should eat enough food." : 
- (bmi<= 25) ?"You are within the ideal weight range.":
- (bmi<= 29.9) ?"you are fat You should control your weight and exercise.":
- "You are overweight. You should see your doctor.";
-console.log("Your BMI is "+bmi+"\n"+okBmi)
+
+if (cm) {
+let kg = Number(prompt("น้ำหนัก"));
+bmi = (kg / (cm * cm)) * 10000
+    if (kg){
+    let okBmi = (bmi <= 18.6) ? "you are too skinny You should eat enough food." :
+        (bmi <= 25) ? "You are within the ideal weight range." :
+            (bmi <= 29.9) ? "you are fat You should control your weight and exercise." :
+                "You are overweight. You should see your doctor.";
+    console.log("Your BMI is " + bmi + "\n" + okBmi)
+}else{console.log("กรุณากรอกเป็นตัวเลข")}
+}else{console.log("กรุณากรอกเป็นตัวเลข")}
