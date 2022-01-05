b2fdd38cd4d82bdf5e5d5d843834814e866b634b
NuengPK <94308487+NuengPK@users.noreply.github.com>
2022-01-05 14:51:41 +0700

    You got it in ?.. guesses

--- assignment39-1.js
+++ assignment39-1.js
@@ -10,29 +10,31 @@ do {
     console.log("Pick a difficulty level (1, 2, or 3) : " + level)
 
     lHint = Math.floor(hint * (10 ** level))
-    //console.log(lHint)
-
+    
+    console.log(lHint)
+    guess = prompt("I have my number. What's your guess?")
     do {
-        guess = prompt("I have my number. What's your guess?")
+    
         if (guess > lHint) {
-            console.log("Too low. Guess again: " + guess)
-            console.log("You got it in " + (4 - g) + " guesses!\n")
+            console.log("Too high. Guess again: " + guess)
+            console.log("You got it in " + g + " guesses!\n")
         } else if (guess == ""){
             console.log("Prompt a number. Guess again ")
-            console.log("You got it in " + (4 - g) + " guesses!\n")
+            console.log("You got it in " + g + " guesses!\n")
         } else if (guess < lHint) {
-            console.log("Too high. Guess again: " + guess)
-            console.log("You got it in " + (4 - g) + " guesses!\n")
+            console.log("Too low. Guess again: " + guess)
+            console.log("You got it in " + g + " guesses!\n")
         } else if (isNaN(guess)){
             console.log(guess + ": Not a number. Guess again ")
-            console.log("You got it in " + (4 - g) + " guesses!\n")
+            console.log("You got it in " + g + " guesses!\n")
         }else if (guess == lHint){
             console.log("True")
             break
         }
         g++
-    } while (guess !== lHint && g < 5)
-
+        guess = prompt("Guess again :")
+    } while (guess !== lHint )
+    
     do{
     playAgain = prompt("Play again?")
     if (playAgain == "n") {
